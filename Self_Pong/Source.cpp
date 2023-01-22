ac6e9d2e42af83f373d72b42be02c998eff369bd
sultanovN <104188736+sultanovN@users.noreply.github.com>
2023-01-22 18:15:20 +0600

    Background color change, instant round start

--- Self_Pong/Source.cpp
+++ Self_Pong/Source.cpp
@@ -2,10 +2,39 @@
 #include <cstdint>
 #include <string>
 const static int s_paddle_padding = 30;
-static const char* text;
+//static const char* text;
+bool b_restart = true;
+
 Sound kick; 
 uint16_t left_player = 0;
 uint16_t right_player = 0;
+
+uint16_t ball_color = 0;
+uint16_t paddle_color = 9;
+uint16_t background_color = 0;
+
+const uint16_t max_colors = 14u;
+const Color colors[max_colors]
+{
+	{ 253, 249, 0, 255 }, { 255, 203, 0, 255 },
+	{ 255, 161, 0, 255 }, { 255, 109, 194, 255 },
+	{ 230, 41, 55, 255 }, { 190, 33, 55, 255 },
+	{ 0, 228, 48, 255 }, { 0, 158, 47, 255 } ,
+	{ 102, 191, 255, 255 }, { 0, 121, 241, 255 },
+	{ 200, 122, 255, 255 }, { 135, 60, 190, 255 },
+	{ 127, 106, 79, 255 }, { 255, 0, 255, 255 }
+};
+const uint16_t background_max = 4;
+
+const Color backgroundColors[background_max]
+{
+	WHITE, BLACK, BEIGE, {0, 255, 255, 1}
+};
+
+Color colorChanger(uint16_t count, const Color colors[])
+{
+	return colors[count];
+}
 
 class Paddle
 {
@@ -17,7 +46,7 @@ private:
 	float m_height;
 	Color m_color;
 public:
-	Paddle(float x, float y, float speed, float width, float height, Color color)
+	Paddle(float x, float y, float speed, float width, float height, Color color = colorChanger(paddle_color, colors))
 		: m_x{x}, m_y{y}, m_speed{speed}, m_width{width}, m_height{height}, m_color{color}
 	{
 
@@ -73,7 +102,8 @@ private:
 	float m_radius;
 	Color m_color;
 public:
-	Ball(float x = GetScreenWidth() / 2.0f, float y = GetScreenHeight() / 2.0f, float speed_x = 150, float speed_y = 260, float radius = 5, Color color = ORANGE)
+	Ball(float x = GetScreenWidth() / 2.0f, float y = GetScreenHeight() / 2.0f, float speed_x = 150, float speed_y = 260, 
+		float radius = 5, Color color = colorChanger(ball_color, colors))
 		: m_x{x}, m_y{y}, m_speed_x{speed_x}, m_speed_y{speed_y}, m_radius{radius}, m_color{color}
 	{
 
@@ -113,20 +143,20 @@ public:
 		
 	}
 
-	void winner()
-	{
-		if (m_x < 0)
-		{
-			text = "Right Player Wins! Press Space To Restart";
-			DrawText(text, GetScreenWidth()/2 - MeasureText(text, 30)/2, GetScreenHeight() / 2, 30, WHITE);
-		}
-		if (m_x > GetScreenWidth())
-		{
-			text = "Left Player Wins! Press Space To Restart";
-			DrawText(text, GetScreenWidth()/2 - MeasureText(text, 30)/2, GetScreenHeight() / 2, 30, WHITE);
-		}
+	//void winner()
+	//{
+	//	if (m_x < 0)
+	//	{
+	//		text = "Right Player Wins! Press Space To Restart";
+	//		DrawText(text, GetScreenWidth()/2 - MeasureText(text, 30)/2, GetScreenHeight() / 2, 30, GRAY);
+	//	}
+	//	if (m_x > GetScreenWidth())
+	//	{
+	//		text = "Left Player Wins! Press Space To Restart";
+	//		DrawText(text, GetScreenWidth()/2 - MeasureText(text, 30)/2, GetScreenHeight() / 2, 30, GRAY);
+	//	}
 
-	}
+	//}
 
 	void isColliding(Paddle left_paddle, Paddle right_paddle)
 	{
@@ -152,33 +182,35 @@ public:
 		}
 	}
 
-	void restart(Paddle &left_paddle, Paddle &right_paddle) 
+	void restart(Paddle &left_paddle, Paddle &right_paddle, uint16_t &ball_color, uint16_t &paddle_color) 
 	{ 
+		++ball_color;
+		++paddle_color;
 		*this = Ball{};
-		left_paddle = Paddle{ s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100, BLUE };
-		right_paddle = Paddle{ GetScreenWidth() * 1.0f - s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100, BLUE };
-		text = nullptr;
+		left_paddle = Paddle{ s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100};
+		right_paddle = Paddle{ GetScreenWidth() * 1.0f - s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100};
+		b_restart = true;
 	}
 
 };
 
 void scoreDraw(Ball ball)
 {
-	if (ball.getX() < 0 && text == nullptr)
+	if (ball.getX() < 0 && b_restart)
 		++right_player;         
 
-	if (ball.getX() > GetScreenWidth() && text == nullptr)
+	if (ball.getX() > GetScreenWidth() && b_restart)
 		++left_player;
 
 	std::string score = std::to_string(left_player) + ':' + std::to_string(right_player);
 
-	DrawText(score.c_str(), GetScreenWidth() / 2 - MeasureText(score.c_str(), 30) / 2, 30, 30, WHITE);
+	DrawText(score.c_str(), GetScreenWidth() / 2 - MeasureText(score.c_str(), 30) / 2, 30, 30, GRAY);
 }
 
 void startText(bool s)
 {
 	if(!s)
-		DrawText("Press Enter To Start", GetScreenWidth() / 2 - MeasureText("Press Enter To Start", 30) / 2, GetScreenHeight() / 2, 30, WHITE);
+		DrawText("Press Enter To Start", GetScreenWidth() / 2 - MeasureText("Press Enter To Start", 30) / 2, GetScreenHeight() / 2, 30, GRAY);
 }
 	
 
@@ -190,6 +222,8 @@ void tutorial(bool tutorial_left, bool tutorial_right)
 		DrawText("UP\nDOWN", GetScreenWidth() - 80, GetScreenHeight() - 80, 20, GRAY);
 }
 
+
+
 int main()
 {
 	#pragma comment(linker, "/SUBSYSTEM:windows /ENTRY:mainCRTStartup")
@@ -199,9 +233,11 @@ int main()
 
 	SetWindowState(FLAG_VSYNC_HINT);
 
+	
+
 	Ball ball{};
-	Paddle leftPaddle{ s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100, BLUE };
-	Paddle rightPaddle{ GetScreenWidth() * 1.0f - s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100, BLUE };
+	Paddle leftPaddle{ s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100};
+	Paddle rightPaddle{ GetScreenWidth() * 1.0f - s_paddle_padding, GetScreenHeight() / 2.0f, 200, 10, 100};
 	
 	bool tutorial_left = 0;
 	bool tutorial_right = 0;
@@ -209,8 +245,19 @@ int main()
 
 	while (!WindowShouldClose())
 	{
+		//Checking is start key pressed
 		if(IsKeyPressed(KEY_ENTER))
 			start = 1;
+
+		//changing colors
+		if (ball_color == max_colors - 1)
+			ball_color = 0;
+		if (paddle_color == max_colors - 1)
+			paddle_color = 0;
+		if (background_color == background_max - 1)
+			background_color = 0;
+
+
 
 		if (start)
 		{
@@ -219,18 +266,22 @@ int main()
 			ball.move();
 			ball.isColliding(leftPaddle, rightPaddle);
 			scoreDraw(ball);
-
-			ball.winner();
+			if (ball.getX() < 0 || ball.getX() > GetScreenWidth())
+				b_restart = false;
 		}
 		
 
 		
 
-		if (IsKeyPressed(KEY_SPACE) && text != nullptr)
-			ball.restart(leftPaddle, rightPaddle);
+		if (!b_restart)
+		{
+			++background_color;
+			ball.restart(leftPaddle, rightPaddle, ball_color, paddle_color);
+		}
+			
 
 		BeginDrawing();
-		ClearBackground(BLACK);
+		ClearBackground(colorChanger(background_color, backgroundColors));
 		
 
 		startText(start);
